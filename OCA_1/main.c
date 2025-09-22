c24dbf788b0364078bbcd36569c8083b106fcf18
ArAversi0 <pancheshny2020@yandex.ru>
2025-09-22 08:20:54 +0300

    implemented_a_simple_sum_task

---
+++ OCA_1/main.c
@@ -0,0 +1,9 @@
+#include "sum_analysis.h"
+#include <windows.h>
+
+int main(void) {
+    SetConsoleOutputCP(CP_UTF8);
+    
+    calculate_sum(); /* вызов простой задачи */
+    return 0;
+}
---
+++ OCA_1/sum_analysis.h
@@ -0,0 +1,6 @@
+#ifndef SUM_ANALYSIS_H
+#define SUM_ANALYSIS_H
+
+void calculate_sum(void); /* O(1) */
+
+#endif /* SUM_ANALYSIS_H */
